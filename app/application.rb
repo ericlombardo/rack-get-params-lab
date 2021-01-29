4bd176c5f0055f12456a07545b1d7687c9daf3ea
Eric <eric.m.lombardo@gmail.com>
2021-01-29 13:46:18 -0500

    Add control flow for cart and cart path

--- app/application.rb
+++ app/application.rb
@@ -1,18 +1,30 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart = []
 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
-
+    
     if req.path.match(/items/)
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+    elsif req.path.match(/cart/)
+      @@cart.empty? ? (resp.write "Your cart is empty") : (@@cart.each {|item| resp.write"#{item}\n"})
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/add/)
+      req.params.each do |key, item|
+        if @@items.include?(item)
+          @@cart << item    
+          resp.write "added #{item}"
+        else
+          resp.write "We don't have that item"
+        end
+      end
     else
       resp.write "Path Not Found"
     end
